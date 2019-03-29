c7f3db05fa93d2388d915fc1cc6234e96172e999
Mike <mikeinhistory@gmail.com>
2019-03-28 22:26:29 -0500

    nothing works and nothing makes sense

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tuna Burger', true);
